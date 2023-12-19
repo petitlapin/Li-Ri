2dcc12c7c82f17e92abdf7ce04aa4e1b81f19b70
Johnny Jazeix <jazeix@gmail.com>
2023-12-19 14:25:05 +0100

    version 3.0.1

--- src/main.cc
+++ src/main.cc
@@ -43,7 +43,7 @@
 SDL_Window *sdlWindow; // Pointe sur l'écran video
 SDL_Renderer *sdlRenderer; // Pointe sur l'écran video
 
-char Titre[]="Li-ri V3.0.0";
+char Titre[]="Li-ri V3.0.1";
 
 Sprite *Sprites=NULL; // Pointe sur les sprites
 int NSprites=0; // Nombre de sprites en mémoire
