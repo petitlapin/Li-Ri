22d98c2161e7b25788fee9b7224b8eed3e2616ff
Johnny Jazeix <jazeix@gmail.com>
2024-01-04 19:06:14 +0100

    bump version to 3.1.1

--- src/main.cc
+++ src/main.cc
@@ -43,7 +43,7 @@
 SDL_Window *sdlWindow; // Pointe sur l'écran video
 SDL_Renderer *sdlRenderer; // Pointe sur l'écran video
 
-char Titre[] = "Li-ri V3.1.0";
+char Titre[] = "Li-ri V3.1.1";
 
 Sprite *Sprites = nullptr; // Pointe sur les sprites
 int NSprites = 0; // Nombre de sprites en mémoire
