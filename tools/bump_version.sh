5678ba916ba2129338e89031c299b7630288aefc
Johnny Jazeix <jazeix@gmail.com>
2024-01-04 19:50:01 +0100

    update bump_version.sh script to also generate changelogs for fastlane

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Bump the version in multiple files such as CMakeLists.txt, AndroidManifest.xml
-# If option "-r <date>" is set, it will also update org.liri.liri.appdata.xml file.
 # If option "-c" is set, it will also regenerate the fastlane changelogs from the NEWS file.
 # There is no check at all if the release already exists or if the values are correct.
 #
@@ -9,9 +8,8 @@
 #   SPDX-License-Identifier: GPL-3.0-or-later
 
 function usage() {
-    echo "Usage: ./tools/bump_version.sh -v <version> [-r <date>] [-h]"
+    echo "Usage: ./tools/bump_version.sh -v <version> [-h]"
     echo "  -v, --version <version>   contains the new version <major>.<minor>.<patch>"
-    echo "  -r, --release <date>      contains the date of the release YYYY-MM-DD (optional)"
     echo "  -c, --changelog           generate fastlane changelog from NEWS (optional)"
     echo "  -h, --help                displays this help"
 }
@@ -26,11 +24,6 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     -v|--version)
       VERSION="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -r|--release)
-      DATE="$2"
       shift # past argument
       shift # past value
       ;;
@@ -66,7 +59,6 @@ patch=`echo $VERSION | cut -d. -f3`
 
 version=${major}.${minor}.${patch}
 echo "Version  = $version"
-echo "Date     = ${DATE}"
 
 # Update CMakeLists LIRI_VERSION_MAJOR and LIRI_VERSION_MINOR variables
 sed -i "s/set(LIRI_VERSION_MAJOR [0-9]\+)$/set\(LIRI_VERSION_MAJOR $major\)/" CMakeLists.txt
@@ -82,16 +74,10 @@ sed -i "s/Li-ri V[0-9.]\+/Li-ri V$version/" src/main.cc
 
 git add CMakeLists.txt android/app/src/main/AndroidManifest.xml src/main.cc && git commit -m "bump version to ${major}.${minor}.${patch}"
 
-if [[ "${DATE}" ]]
-then
-    # Add a new release in org.liri.liri.appdata.xml with the new date and release
-    sed -i -e "/<releases>/a\ \ \ \ <release version=\"${major}.${minor}.${patch}\" date=\"$DATE\"/>" org.liri.liri.appdata.xml
-    git add org.liri.liri.appdata.xml && git commit -m "appdata, add ${major}.${minor}.${patch} release"
-fi
-
-# TODO
 if [[ "${CHANGELOG}" ]]
 then
-    #python tools/fdroid_update_fastlane_metadata.py
-    echo "todo"
+    appstreamcli news-to-metainfo ./NEWS.yaml ./org.liri.liri.appdata.xml
+    python tools/fdroid_create_fastlane_changelogs.py
+
+    git add fastlane/metadata/android/en-US/changelogs/ org.liri.liri.appdata.xml && git commit -m "update changelog for ${major}.${minor}.${patch}"
 fi
