1c2fa63b87131ace1a0c488c33bd1762bd3678d3
Johnny Jazeix <jazeix@gmail.com>
2024-01-15 21:21:18 +0100

    adapt simpleini path for fdroid

--- android/app/src/main/jni/src/Android.mk
+++ android/app/src/main/jni/src/Android.mk
@@ -7,7 +7,8 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL2
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/ \
-					$(LOCAL_PATH)/../SDL2_mixer \
+		    $(LOCAL_PATH)/../SDL2_mixer \
+		    $(LOCAL_PATH)/../simpleini \
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
